5cfa86f60a47326d6335369ad7dfb34bcd1daea6
Henshaw, Andy <Andy.Henshaw@gtri.gatech.edu>
2019-08-12 01:17:52 -0400

    removed notebook interaction

--- db1.py
+++ db1.py
@@ -1,10 +1,10 @@
 ''' db1 - Demonstrate importing our own module '''
-#%%
+import matplotlib.pyplot as plt
 # custom modules
 from load_and_clean import load_raw, clean
 
 df = clean(load_raw())
 print(df.describe())
 print(df.head())
-#%%
 df.AGE.plot(kind='hist')
+plt.show()
