8092182826f90728bc9aecc07bb67049980bf09d
Henshaw, Andy <Andy.Henshaw@gtri.gatech.edu>
2019-08-12 00:53:27 -0400

    csv

--- scrape_html1.py
+++
@@ -1,9 +0,0 @@
-''' scrape_html1 - Exploration'''
-
-# third-party libraries
-import requests
-
-url = 'http://graduate.kennesaw.edu/datascience/students.php'
-
-response = requests.get(url)
-print(response.content)
